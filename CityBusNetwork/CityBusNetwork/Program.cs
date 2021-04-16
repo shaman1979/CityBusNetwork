bd168e6f78e7b7eb004609f3d09b1867f9cd7687
shaman1979 <upgamestudio@gmail.com>
2021-04-16 13:52:39 +0300

    Создал проект

---
+++ CityBusNetwork/CityBusNetwork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CityBusNetwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
